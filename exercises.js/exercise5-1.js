d9c1fd66519ce21e4a6cb3a6fe8850eb282453a4
LuLuMS0206 <luciamacho00@gmail.com>
2024-04-11 19:40:47 +0200

    exercises5-1

--- exercises.js/exercise5-1.js
+++ exercises.js/exercise5-1.js
@@ -5,5 +5,25 @@ let miMatriz = [];
 let places = ["Bali", "Punta cana", "Paris", "Riviera Maya", "Venecia", "Islas griegas"];
 
 //3
-let longitud = places.length;
-console.log("La longitud de la matriz es: " + longitud);
+
+let longitud = 0;
+
+for (let i = 0; i < places.length; i++) {
+    longitud++;
+}
+
+console.log("La longitud de la matriz es:", longitud);
+
+//4 
+// Obtengo el primer elemento
+let primerElemento = places[0];
+
+// Obtengo el elemento intermedio 
+let elementoIntermedio = places[Math.floor(places.length / 2)];
+
+// Obtengo el último elemento
+let ultimoElemento = places[places.length - 1];
+
+console.log("Primer elemento:", primerElemento);
+console.log("Elemento intermedio:", elementoIntermedio);
+console.log("Último elemento:", ultimoElemento);
