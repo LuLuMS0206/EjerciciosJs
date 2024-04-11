8f8f88c09c017621ecf02a2aed20ab91181d8d75
LuLuMS0206 <luciamacho00@gmail.com>
2024-04-11 20:31:48 +0200

    exercise5-2

---
+++ exercises.js/exercise5-2.js
@@ -0,0 +1,23 @@
+//3
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+
+// agregue 'Meat' al principio de su carrito de compras si aún no se ha agregado
+
+if (!shoppingCart.includes('Meat')) {
+    shoppingCart.push('Meat');
+    shoppingCart.reverse(); // para invertir el orden de los elementos
+}
+
+console.log(shoppingCart);
+
+// agregue Sugar al final de su carrito de compras si aún no se ha agregado
+
+shoppingCart.push('Sugar')
+console.log(shoppingCart)
+
+// elimine 'Miel' si es alérgico a la miel
+
+// modificar té a 'té verde'
+
+
+//6
