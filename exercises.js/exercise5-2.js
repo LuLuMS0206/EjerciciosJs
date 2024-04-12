d296fac0d42321c0965fa7555adb2a138c86a27e
LuLuMS0206 <luciamacho00@gmail.com>
2024-04-12 11:56:28 +0200

    exercise5-2

--- exercises.js/exercise5-2.js
+++ exercises.js/exercise5-2.js
@@ -10,14 +10,27 @@ if (!shoppingCart.includes('Meat')) {
 
 console.log(shoppingCart);
 
-// agregue Sugar al final de su carrito de compras si aún no se ha agregado
+// // agregue Sugar al final de su carrito de compras si aún no se ha agregado
 
 shoppingCart.push('Sugar')
 console.log(shoppingCart)
 
 // elimine 'Miel' si es alérgico a la miel
 
+shoppingCart.pop() 
+console.log(shoppingCart) 
+
+
 // modificar té a 'té verde'
 
+shoppingCart[2] = 'Green Tea'  
+
+console.log(shoppingCart) 
 
 //6
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+const fullStack = frontEnd.concat(backEnd)
+
+console.log(fullStack)
