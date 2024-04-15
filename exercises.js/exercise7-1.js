bdf414c09a5e90e657b5581c0f432ba58b528afc
LuLuMS0206 <luciamacho00@gmail.com>
2024-04-15 09:46:18 +0200

    exercise7-1

---
+++ exercises.js/exercise7-1.js
@@ -0,0 +1,26 @@
+//1y2
+function fullName() {
+    const nombre = "Lucia";
+    const apellido = "Macho";
+    console.log(nombre + " " + apellido);
+}
+fullName();
+
+//3
+function addNumbers() {
+    const numerOne = 3;
+    const numerTwo = 6;
+    console.log(numerOne + numerTwo)
+}
+
+addNumbers()
+
+//4
+function areaOfRectangle() {
+    const largo = 5;
+    const ancho = 3;
+    console.log("El área del rectángulo es:", largo * ancho);
+}
+
+
+areaOfRectangle();
